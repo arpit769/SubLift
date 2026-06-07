acc7ed4003ca9ed614076f277d19c364d8897db9
ARPIT BHUSHAN MAURYA <hy196193@gmail.com>
2026-06-07 15:30:22 +0530

    Add robots.txt configuration for SEO

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://sub-lift.vercel.app/sitemap.xml',
+  }
+}
