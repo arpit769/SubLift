9d446def6186242eb4063fe1c518597170ff7652
ARPIT BHUSHAN MAURYA <hy196193@gmail.com>
2026-07-15 11:14:38 +0530

    Update sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://sub-lift.vercel.app/sitemap.xml',
+    sitemap: 'https://growmysub.com/sitemap.xml',
   }
 }
