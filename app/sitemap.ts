f59faba88878da4dfd851b75a661d6d2ac0026fa
ARPIT BHUSHAN MAURYA <hy196193@gmail.com>
2026-07-15 11:07:09 +0530

    Update sitemap URL to new domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://sub-lift.vercel.app',
+      url: 'https://growmysub.com',
       lastModified: new Date(),
     },
   ]
